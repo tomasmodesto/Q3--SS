550794f5526a5ab2ce502b4ffac730fe66e31448
Tomás Modesto <88251894+tomasmodesto@users.noreply.github.com>
2024-01-05 00:18:28 +0000

    musicagrupo33.py

--- musicagrupo33.py
+++ musicagrupo33.py
@@ -1,7 +1,8 @@
+#Música - Grupo 33
+
 #declaração de varíaveis das notas musicais
 
 midtone = 2**(1/12)
-
 la4 = 440.00
 la4s = la4 * midtone
 si4 = la4s * midtone
@@ -22,12 +23,6 @@ sol3 = sol4/2
 la3 = la4/2
 si3 = si4/2
 
-
-
-
-
-
-
 samplingrate = 16000
 t=timevar(1)
 x = seqsin(0, 0.7, sol4, 0.1, fa4s, 0.1, sol4, 0.2, si4, 0.1, do5, 0.1, si4, 0.8, 0, 0.2, fa4s, 0.1, sol4, 0.1, fa4s, 0.2, sol4, 0.1, la4, 0.1, sol4, 0.8, 0, 0.2, fa4s, 0.1, mi4, 0.1, fa4s, 0.2, si4, 0.1, do5, 0.1, si4, 0.8, 0, 0.2, fa4s, 0.1, mi4, 0.1, fa4s, 1.0, 0, 0.7)
@@ -36,6 +31,5 @@ x = seqsin(0, 0.7, sol4, 0.1, fa4s, 0.1, sol4, 0.2, si4, 0.1, do5, 0.1, si4, 0.8
 y = seqsin(0, 0.5, mi4, 0.2, si3, 0.2, mi4, 0.2, si3, 0.2, mi4, 0.2, si3, 0.2, mi4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, si3, 0.2, re4, 0.2, la3, 0.2, re4, 0.2, la3, 0.2, re4, 0.2, la3, 0.2, re4, 0.2, la3, 0.2, 0, 0.5)   
 
 z = seqsin(0, 0.5, mi3, 0.4, sol3, 0.4, mi3, 0.4, sol3, 0.4, re3, 0.4, sol3, 0.4, re3, 0.4, sol3, 0.4, re3, 0.4, fa3s , 0.4, re3, 0.4, fa3s, 0.4, re3, 0.4, fa3s, 0.4, re3, 0.4, fa3s, 0.4, 0, 0.5)
-
 
 play(0.5*x + 0.25*y + 0.25*z)
